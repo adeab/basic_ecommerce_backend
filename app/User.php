a18421eeeb53ff74af8807f64cb465744112b5e5
adeab <adeab@live.com>
2021-09-29 18:15:20 +0600

    jwt setup

--- app/User.php
+++ app/User.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+
 
 class User extends Authenticatable
 {
@@ -36,4 +38,31 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+    * Get the identifier that will be stored in the subject claim of the JWT.
+    *
+    * @return mixed
+    */
+   public function getJWTIdentifier()
+   {
+       return $this->getKey();
+   }
+ 
+   /**
+    * Return a key value array, containing any custom claims to be added to the JWT.
+    *
+    * @return array
+    */
+   public function getJWTCustomClaims()
+   {
+       return [];
+   }
+ 
+   public function setPasswordAttribute($password)
+   {
+       if ( !empty($password) ) {
+           $this->attributes['password'] = bcrypt($password);
+       }
+   }
 }
