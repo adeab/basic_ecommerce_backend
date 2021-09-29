0adbe7f2744581dcc7e7bc57a4f8e509ee69e969
adeab <adeab@live.com>
2021-09-30 01:44:38 +0600

    jwt bug fix

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
